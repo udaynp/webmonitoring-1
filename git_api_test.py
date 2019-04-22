4b988613f8a2abe9bd7f1544e8d4e0323b8ed381
uday <udaygulf@gmail.colm>
2019-04-22 01:14:13 -0700

    Update git_api_test.py

--- git_api_test.py
+++ git_api_test.py
@@ -2,9 +2,11 @@ import json
 import requests
 
 username = 'udayradhika'
-token = 'd9a5eee4961340145d256c011e2a311185980c9d'
+token = 'c3b7b60e52411292c7c931c66d968a79851750cc'
+#token = 'coppergate51'
+
 headers = {'Authorization': 'token ' + token}
-repo = 'webmonitoring'
+repo_name = 'webmonitoring'
 
 login = requests.get('https://api.github.com/user', headers=headers)
 
@@ -20,20 +22,29 @@ def make_github_issue(title, body=None, labels=None):
     headers = {'Authorization': 'token ' + token}
 
     #login = requests.get('https://api.github.com/user', headers=headers)
-    login = 'https://api.github.com/' + 'repos/' + username + '/' + repo+'/issues/'
+    url = requests.get('https://api.github.com/repos/'+username+'/'+repo_name+'/issues',headers=headers)
     # Create an authenticated session to create the issue
-    gh_session = requests.Session()
-    gh_session.auth = (username, token)
     # Create our issue
     issue = {'title': title,
              'body': body,
              'labels': labels}
     # Add the issue to our repository
-    r = session.post(login, json.dumps(issue))
+    r = requests.post(url, json.dumps(issue))
     if r.status_code == 201:
         print ('Successfully created Issue {0:s}'.format(title))
     else:
         print ('Could not create Issue {0:s}'.format(title))
         print ('Response:', r.content)
 
-make_github_issue(title="url", body="test", labels="response")
+#make_github_issue(title="url", body="test", labels="response")
+
+
+
+
+payload = { "title": "response time is more ","body": "response time is more for url1","labels": ["bug"]}
+
+login = requests.post('https://api.github.com/'+'repos/'+username+'/'+repo_name+'/issues', auth=(username,token), data=json.dumps(payload))
+print(login.json())
+
+
+
