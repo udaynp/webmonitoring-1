749ad2c318b1dbcfff4e01cd771e950cfe24a8a8
udayradhika <49745134+udayradhika@users.noreply.github.com>
2019-05-03 23:45:49 -0700

    Update crafts_demo_site.sh

--- crafts_demo_site.sh
+++ crafts_demo_site.sh
@@ -3,6 +3,7 @@ FLASK_APP=flask_app.py flask run  > /tmp/main_flask_app.out 2>&1 &
 
 while :; do
   python refresh_app.py
+  sleep 5
   if [ $? -ne 0 ]; then
     echo "site is not running -- app"
     break
